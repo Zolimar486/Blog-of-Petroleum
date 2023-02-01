d875d436ddb32d1fb2be6dcde2c5facd4e2119ce
Zolimar486 <ing.carrasquero.zolimar@gmail.com>
2023-02-01 20:22:09 -0300

    version 1

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors= require("cors")
+const path = require("path")
 
 const app= express();
 app.use(cors());
@@ -14,6 +15,11 @@ mongoose.set('strictQuery', false);
 dotenv.config();
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({extended: true , limit:'50mb'}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
